4197fb3e3d00716c8b2d0686d3286226a4674c94
rimuspp <danellise@outlook.com>
2023-01-27 18:28:05 +0000

    Added Auth Service to this

--- Web.Public/src/App/index.ts
+++ Web.Public/src/App/index.ts
@@ -1,3 +1,6 @@
 export * from './Common';
+export * from './Common/Services/Auth/Custom/AuthService';
+export * from './Common/Helpers/active';
 export * from './Models';
+export * from './Filters/Products';
 export * from './Service';
